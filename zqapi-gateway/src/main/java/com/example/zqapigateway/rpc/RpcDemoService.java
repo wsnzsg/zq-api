636e53ea2415437cd29abab0d8c389c743c18cfc
wsnzsg <140715820+wsnzsg@users.noreply.github.com>
2024-09-12 22:53:33 +0800

    网关bug修复

--- zqapi-gateway/src/main/java/com/example/zqapigateway/rpc/RpcDemoService.java
+++ zqapi-gateway/src/main/java/com/example/zqapigateway/rpc/RpcDemoService.java
@@ -1,5 +1,7 @@
 package com.example.zqapigateway.rpc;
 
+import org.apache.dubbo.config.annotation.DubboReference;
+
 public interface RpcDemoService {
     String sayHello(String name);
 }
--- zqapi-gateway/src/test/java/com/example/zqapigateway/ZqapiGatewayApplicationTests.java
+++ zqapi-gateway/src/test/java/com/example/zqapigateway/ZqapiGatewayApplicationTests.java
@@ -4,12 +4,18 @@ package com.example.zqapigateway;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.zq.project.rpc.RpcDemoService;
 
 @SpringBootTest
 class ZqapiGatewayApplicationTests {
 
+    @DubboReference
+    private RpcDemoService rpcDemoService;
+
     @Test
-    void contextLoads() {
+    void getName() {
+        rpcDemoService.sayHello("zq");
     }
+
 
 }
